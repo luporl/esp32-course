d90a82dc106da2b378c99cbe104c87c250fa652c
luporl <leandro.lupori@gmail.com>
2022-10-21 17:00:41 -0300

    Fix/port hello world/chip info example

--- _3_first_look/_2_6_using_examples_chip_data_2/main/hello_world_main.c
+++ _3_first_look/_2_6_using_examples_chip_data_2/main/hello_world_main.c
@@ -7,35 +7,47 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_mac.h"
 
 void app_main()
 {
-
     printf("Hello world!\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
+    uint32_t flash_size;
+
     esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
+           CONFIG_IDF_TARGET,
            chip_info.cores,
            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
     printf("silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
+    }
+    printf("%ldMB %s flash\n", flash_size / (1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %ld bytes\n", esp_get_minimum_free_heap_size());
 
     printf("idf version is %s\n", esp_get_idf_version());
 
-    uint8_t chipId[6];
-    esp_efuse_mac_get_default(chipId);
-    printf("mac / chip id: %02x%02x%02x%02x%02x%02x\n", chipId[0], chipId[1], chipId[2], chipId[3], chipId[4], chipId[5]);
-
+    uint8_t mac[6];
+    esp_efuse_mac_get_default(mac);
+    printf("mac is ");
+    for (int i = 0; i < 6; i++)
+        printf("%02x", mac[i]);
+    printf("\n");
+    
     for (int i = 10; i >= 0; i--)
     {
         printf("Restarting in %d seconds...\n", i);
